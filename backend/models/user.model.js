7dea9870a60f66a608254da062ca97f48e2fade0
Rohan Bhangale <rohanbhangale25@gmail.com>
2024-12-22 10:24:47 +0530

    file set up

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema =mongoose.Schema({
-    username:{
+    fullname:{
         type:String,
         required:true,
         unique:true,
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-
+import { Route , Routes} from "react-router-dom"
+import SignupPage from "./pages/SignupPage"
 const App = () => {
   return (
     <>
-      
+      <div>
+        <Routes>
+          <Route path="/signup" element={<SignupPage/>}/>
+        </Routes>
+      </div>
     </>
   )
 }

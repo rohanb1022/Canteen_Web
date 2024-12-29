de6a8cfb2fd087c6c2eeb47c4c69f1260718c6c2
Ayushkaranth <ayushkaranth2005@gmail.com>
2024-12-29 20:28:25 +0530

    Resolved conflicts and updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
 import { Route , Routes} from "react-router-dom"
 import SignupPage from "./pages/SignupPage"
 import AuthScreen from "./pages/home/AuthScreen"
-import LoginPage from "./pages/login"
-import HomeScreen from "./pages/home/HomeScreen"
 import Orderlist from "./pages/orderList"
+import HomeScreen from "./pages/home/HomeScreen"
+import LoginPage from "./pages/login"
 import Products from "./pages/products"
+import toast, { Toaster } from "react-hot-toast"; // Import Toaster
 
 const App = () => {
   return (
     <>
       <div>
-        
+      <Toaster position="top-right" reverseOrder={false} />
         <Routes>
           <Route path="/" element={<AuthScreen />} />
           <Route path="/signup" element={<SignupPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/home" element={<HomeScreen/>}/>
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/home" element={<HomeScreen />}/>
           <Route path="/list" element={<Orderlist />}/>
-          <Route path="/products" element={<Products />}/>
+          <Route path="/product" element={<Products />}/>
+
           
           
         </Routes>

107890f5738dc1295159cd64ae474a330715473f
Rohan Bhangale <rohanbhangale25@gmail.com>
2025-01-25 13:26:29 +0530

    added backend side for order history for app users

--- backend/controllers/App/orderHistory.controller.js
+++ backend/controllers/App/orderHistory.controller.js
@@ -1,13 +1,19 @@
 import AppUser from "../../models/appuser.model.js";
 
 export const getOrderHistory = async (req , res) => {
-    const studentId = req.params.id;
+    const studentId = req.user._id;
     try {
         const student = AppUser.findById(studentId);
         if(!student){
-            return res.status(400).json({message : "User is not fount"})
+            res.status(400).json({message : "User not found"})
         }
+        const completedOrder = student.completedOrder;
+        if(!completedOrder){
+            return res.json({message : "No Completed orders found"})
+        }
+        res.json(completedOrder);
     } catch (error) {
-        
+        console.log(error.message , "The error is coming from getOrderHistory function of app side");
+        return res.status(501).json({message : "Internal server error"})
     }
 }

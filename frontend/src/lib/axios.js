b9e63f28318fe5505c71ad60d60d47415d9cbf3a
ayush karanth <ayushkaranth2005@gmail.com>
2025-01-31 12:56:32 +0530

    backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL :  "http://localhost:5000" ,
+    baseURL :  "https://canteen-web.onrender.com" ,
     withCredentials : true
 })

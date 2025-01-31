9d266554b6f2da9283b0c5dd23accb25dc0adab7
ayush karanth <ayushkaranth2005@gmail.com>
2025-01-31 14:42:11 +0530

    url removed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL :  "https://canteen-web.onrender.com" ,
+    baseURL :  "http://localhost:5000" ,
     withCredentials : true
 })

7eb256802ee69b26bd5f0fa2bde99325c23ea7b4
ayush karanth <ayushkaranth2005@gmail.com>
2025-02-04 22:50:52 +0530

    app side changes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,8 @@ import { updateOrderStatus } from '../controllers/orderStatus.controller.js';
 
 const router = express.Router();
 
-router.put('/update-status', updateOrderStatus);
+// router.put('/update-status', updateOrderStatus);
+// Route for fetching ongoing orders view paste this in viewOrder.route.js
+router.get('/view-orders/:userId',  getOrderCards);
 
 export default router;

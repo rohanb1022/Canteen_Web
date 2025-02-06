008c88b92192ee3a1d76701b401015ce765d174f
ayush karanth <ayushkaranth2005@gmail.com>
2025-02-06 23:31:21 +0530

    timeout

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -38,7 +38,7 @@ function HomeScreen() {
   useEffect(() => {
     const interval = setInterval(() => {
       window.location.reload(); // Refreshes the page
-    }, 3000); // 3000 milliseconds = 3 seconds
+    }, 30000); // 3000 milliseconds = 3 seconds
 
     // Clean up the interval on component unmount
     return () => clearInterval(interval);

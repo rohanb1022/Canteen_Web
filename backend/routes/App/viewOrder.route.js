1c5a03398f2c17830d31e562181ecbfc961c4207
ayush karanth <ayushkaranth2005@gmail.com>
2025-02-07 14:18:14 +0530

    latest change

--- backend/routes/App/viewOrder.route.js
+++ backend/routes/App/viewOrder.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { getOrderCards } from '../../controllers/orders.controller.js'; 
 import protectRoute from '../../middleware/App/protectRoute.js';
+import { getOrderStatus } from '../../controllers/orderStatus.controller.js';
+
 const router = express.Router();
 
 // Route for fetching orders in card format
 // router.get('/view-orders',protectRoute ,  getOrderCards);
-router.get('/view-orders/:userId',  getOrderCards);
+router.get('/view-orders/:userId',  getOrderStatus);
 
 export default router;

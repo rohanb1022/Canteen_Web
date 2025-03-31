1ab30c48932dd0dd8a7578389bc74e5ec83d7f9e
prakhar362 <prakharshri2005@gmail.com>
2025-03-31 13:17:59 +0530

    Update order.model.js

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -7,7 +7,7 @@ const orderSchema = new mongoose.Schema({
       foodItemId: { type: mongoose.Schema.Types.ObjectId, ref: 'FoodItem', required: true },
       quantity: { type: Number, required: true },
       price: { type: Number, required: true },
-      status: { type: String, enum: ['accepted', 'rejected'], default: 'pending' }
+      status: { type: String, enum: ['accepted', 'rejected','pending'], default: 'pending' }
     },
   ],
   totalAmount: { type: Number, required: true },
